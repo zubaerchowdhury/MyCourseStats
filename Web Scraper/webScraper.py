60c5869630bc6599048e608cee9b38a92862204e
Marcos Morales <89746298+MarcosM412@users.noreply.github.com>
2024-10-23 20:51:57 -0400

    Grabbed class information for every subject
    Also cleaned up code and added execution time

--- Web Scraper/webScraper.py
+++ Web Scraper/webScraper.py
@@ -1,19 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
+import time
 
-
-def wait_until_timeout(driver):
-    return False
+start_time = time.time()
 
 with webdriver.Firefox() as driver:
 
     driver.get("https://canelink.miami.edu/psp/UMIACP1D/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_Main")
     wait = WebDriverWait(driver, 10)
-    driver.switch_to.frame("TargetContent")
+    driver.switch_to.frame("TargetContent")        
+
+    def clickSearchButton():
+        searchButton = driver.find_element(By.XPATH, "//button[@type='submit']")
+        searchButton.click()
+        wait.until(presence_of_element_located((By.XPATH, "//main//div[2]//hr")))
+                   
+    def getAllClasses():
+        form = driver.find_element(By.XPATH, "//form")
+        classesDiv = form.find_element(By.XPATH, "./following-sibling::div")
+        classes = classesDiv.find_elements(By.XPATH, "./div[@class='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12']")
+        classes.pop(0) # Remove first element because it is the header
+        for c in classes:
+            className = c.find_element(By.TAG_NAME, 'h2').text
+            classInfo = c.find_elements(By.XPATH, ".//span[@class='sr-only']")
+            classInfo.pop(0) # Remove useless element
+            print(className)
+            for info in classInfo:
+                print(info.text, end="; ")
+            print()
+            print()
 
     # Set term to Spring 2025
     termDropdown = driver.find_element(By.XPATH, "//form//div[2]//button")
@@ -21,6 +38,7 @@ with webdriver.Firefox() as driver:
     termDropdownList = driver.find_element(By.XPATH, "//form//div[2]//ul")
     termDropdownListItems = termDropdownList.find_elements(By.TAG_NAME, 'li')
     for item in termDropdownListItems:
+        driver.execute_script("arguments[0].scrollIntoView();", item)
         if item.text == "Spring 2025":
             item.click()
             break
@@ -36,37 +54,26 @@ with webdriver.Firefox() as driver:
             item.click()
             break
 
-    # Set subject to Electrical & Computer Engineer
-    subjectDropdown = driver.find_element(By.XPATH, "//form//div[4]//button[@class='MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator']")
-    driver.execute_script("arguments[0].scrollIntoView();", subjectDropdown)
-    subjectDropdown.click()
-    subjectDropdownList = driver.find_element(By.XPATH, "//form//div[4]//ul")
-    subjectDropdownListItems = subjectDropdownList.find_elements(By.TAG_NAME, 'li')
-    for item in subjectDropdownListItems:
-        driver.execute_script("arguments[0].scrollIntoView();", item)
-        if item.text == "Electrical & Computer Engineer":
-            item.click()
-            break
-
-    # Uncheck show open classes only
+    # Uncheck Show Open Classes Only Checkbox
     showOpenClassesOnlyCheckbox = driver.find_element(By.XPATH, "//input[@type='checkbox']")
     showOpenClassesOnlyCheckbox.click()
 
-    # Click search
-    searchButton = driver.find_element(By.XPATH, "//button[@type='submit']")
-    searchButton.click()
-    wait.until(presence_of_element_located((By.XPATH, "//main//div[2]//hr")))
 
-    # Get all classes
-    form = driver.find_element(By.XPATH, "//form")
-    classesDiv = form.find_element(By.XPATH, "./following-sibling::div")
-    classes = classesDiv.find_elements(By.XPATH, "./div[@class='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12']")
-    classes.pop(0)
-    for c in classes:
-        className = c.find_element(By.TAG_NAME, 'h2').text
-        classInfo = c.find_elements(By.XPATH, ".//span[@class='sr-only']")
-        print(className)
-        for info in classInfo:
-            print(info.text, end=" ")
-        print()
-        
+    # Go through all subjects
+    subjectDropdown = driver.find_element(By.XPATH, "//form//div[4]//button[@class='MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator']")
+    subjectDropdown.click()
+    subjectDropdownList = driver.find_element(By.XPATH, "//form//div[4]//ul")
+    subjectDropdownListItems = subjectDropdownList.find_elements(By.TAG_NAME, 'li')
+    subjectListLength = len(subjectDropdownListItems)
+    academicCareerDropdown.click() # Close subject dropdown by clicking on academic career dropdown
+    for i in range(subjectListLength):
+        subjectDropdown.click()
+        subjectDropdownList = driver.find_element(By.XPATH, "//form//div[4]//ul")
+        subjectDropdownListItems = subjectDropdownList.find_elements(By.TAG_NAME, 'li')
+        item = subjectDropdownListItems[i]
+        driver.execute_script("arguments[0].scrollIntoView();", item)
+        item.click()
+        clickSearchButton()
+        getAllClasses()
+
+print("--- Executed in %s seconds ---" % (time.time() - start_time))
