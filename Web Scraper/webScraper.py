9b5902f8fcccf5d77f43c7d1369aa135ac7d030d
MarcosM412 <marcos.javier.morales.fl@gmail.com>
2024-10-25 18:50:14 -0400

    Added course class

--- Web Scraper/webScraper.py
+++ Web Scraper/webScraper.py
@@ -3,14 +3,59 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import time
+import datetime
 
+class course:
+    #semesters = ["Spring", "Summer", "Fall", "Non-credit Term"]
+    #sessions = ["Regular Academic", "Summer Session A 5W", "Summer Scholars Program"]
+    #status = ["Open", "Closed", "Waitlist"]
+
+    def __init__(self):
+        self.name = ""
+        self.subject = ()
+        self.catalogNumber = ""
+        self.semester = ""
+        self.year = 0
+        self.sectionType = ""
+        self.sectionCode = ""
+        self.classNumber = ""
+        self.session = ""
+        self.days = [""]
+        self.timeStart = datetime.time()
+        self.timeEnd = datetime.time()
+        self.classroom = ""
+        self.instructor = [""]
+        self.startDate = datetime.date()
+        self.endDate = datetime.date()
+        # self.units = ""
+        self.status = ""
+        self.seatsAvailable = ""
+        self.capacity = ""
+        self.waitlistAvailable = ""
+        self.waitlistCapacity = ""
+        self.notes = ""
+        # self.decription = ""
+        # self.prerequisites = ""
+
+    def __repr__(self):
+        return (f"Name: {self.name}, Subject: {self.subject}, Catalog Number: {self.catalogNumber}, "
+            f"Semester: {self.semester}, Year: {self.year}, Section Type: {self.sectionType}, "
+            f"Section Code: {self.sectionCode}, Class Number: {self.classNumber}, Session: {self.session}, "
+            f"Days: {self.days}, Time Start: {self.timeStart}, Time End: {self.timeEnd}, "
+            f"Classroom: {self.classroom}, Instructor: {self.instructor}, Start Date: {self.startDate}, "
+            f"End Date: {self.endDate}, Status: {self.status}, Seats Available: {self.seatsAvailable}, "
+            f"Capacity: {self.capacity}, Waitlist Available: {self.waitlistAvailable}, "
+            f"Waitlist Capacity: {self.waitlistCapacity}, Notes: {self.notes}")
+    
 start_time = time.time()
 
 with webdriver.Firefox() as driver:
 
     driver.get("https://canelink.miami.edu/psp/UMIACP1D/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_Main")
     wait = WebDriverWait(driver, 10)
-    driver.switch_to.frame("TargetContent")        
+    driver.switch_to.frame("TargetContent")    
+    currentTerm = ""
+    currentAcademicCareer = ""   
 
     def clickSearchButton():
         searchButton = driver.find_element(By.XPATH, "//button[@type='submit']")
@@ -32,7 +77,8 @@ with webdriver.Firefox() as driver:
             print()
             print()
 
-    def setAcademicCareer(academicCareer):
+    def setAcademicCareer(academicCareer: str):
+        currentAcademicCareer = academicCareer
         academicCareerDropdown = driver.find_element(By.XPATH, "//form//div//div[3]//button[@class='MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator']")
         academicCareerDropdown.click()
         academicCareerDropdownList = driver.find_element(By.XPATH, "//form//div[3]//ul")
@@ -42,7 +88,8 @@ with webdriver.Firefox() as driver:
                 item.click()
                 break
 
-    def setTerm(term):
+    def setTerm(term: str):
+        currentTerm = term
         termDropdown = driver.find_element(By.XPATH, "//form//div[2]//button")
         termDropdown.click()
         termDropdownList = driver.find_element(By.XPATH, "//form//div[2]//ul")
@@ -58,7 +105,7 @@ with webdriver.Firefox() as driver:
         showOpenClassesOnlyCheckbox.click()
 
     def getAllSubjects():
-        # Go through all subjects
+        # Go through all subjects and get all classes
         subjectDropdown = driver.find_element(By.XPATH, "//form//div[4]//button[@class='MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator']")
         subjectDropdown.click()
         subjectDropdownList = driver.find_element(By.XPATH, "//form//div[4]//ul")
