0ec1d8b6140e24ba1fe2ef6ee3faa2e4d0c4d453
hpenano <hcm.penano@gmail.com>
2025-04-13 18:48:33 -0400

    fixed search button in homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -113,13 +113,14 @@ function Home() {
                             placeholder="e.g. 101"
                             className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                           />
-                          <button
+                      </div>
+                      <div className="relative flex-1">
+                      <button
                             type="submit"
                             className="ml-2 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                           >
                             Search
-                          </button>
-                        
+                          </button>   
                       </div>
                     </div>
                     
