203351a273a955cf981d2d6d1f7e51596b16fd32
Zubaer Chowdhury <zrchy16@gmail.com>
2025-04-19 23:59:49 -0400

    Rounded MCS logo and MyCourseStats redirect to Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,13 +22,15 @@ function App() {
                     style={{ height: "40px" }}
                   />
                   <span className="ml-2 text-xl font-bold text-gray-900">
-                    MyCourseStats
+                    <Link to="/" className="hover:text-gray-900">
+                      MyCourseStats
+                    </Link>
                   </span>
                 </div>
                 <div className="hidden md:flex items-center space-x-8">
                   <Link
                     to="/about"
-                    className="block px-3 py-2 text-base font-medium text-gray-600 hover:text-gray-900"
+                    className="block px-3 py-2 text-base font-medium text-gray-700 hover:text-gray-900"
                   >
                     About
                   </Link>
